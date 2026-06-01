a26722e89f07ddc3d05fc566dfa1d525238ff778
tonicfilip <65338128+tonicfilip@users.noreply.github.com>
2026-06-01 13:03:24 +0200

    env fix

--- server/alembic/env.py
+++ server/alembic/env.py
@@ -4,10 +4,10 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-# from models.case import Base as CaseBase
-# from models.step import Base as StepBase
 from models.base import Base
 import models
+import os
+from config import get_database_url
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -42,7 +42,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = get_database_url()
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -61,8 +61,11 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+    db_url = get_database_url()
+    configuration = {"sqlalchemy.url": db_url}
+
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        configuration,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
@@ -80,3 +83,4 @@ if context.is_offline_mode():
     run_migrations_offline()
 else:
     run_migrations_online()
+
